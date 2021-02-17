fa0934527a37661c2162f39b571c98d6887fef5f
samara-amanda <samara.amanda01@gmail.com>
2021-02-17 09:07:01 -0500

    replaced restaurants table

---
+++ db/migrate/20210217120338_create_reviews.rb
@@ -0,0 +1,12 @@
+class CreateReviews < ActiveRecord::Migration[6.0]
+  def change
+    create_table :reviews do |t|
+      t.string :title
+      t.string :content
+
+      t.belongs_to :user, index: true, foreign_key: true
+      t.belongs_to :restaurant, index: true, foreign_key: true 
+
+    end
+  end
+end
---
+++ db/migrate/20210217132533_create_restaurants.rb
@@ -0,0 +1,9 @@
+class CreateRestaurants < ActiveRecord::Migration[6.0]
+  def change
+    create_table :restaurants do |t|
+      t.string :name        
+      t.belongs_to :review, index: true,  foreign_key: true 
+      t.belongs_to :category, index: true, foreign_key: true
+    end
+  end
+end
