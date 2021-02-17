6e287b273735a35cb93f9b9e7fbd23b03edb8d6e
samara-amanda <samara.amanda01@gmail.com>
2021-02-17 06:36:44 -0500

    #new, #create, #index

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,4 +1,27 @@
-class RestaurantController < ApplicationController 
+class RestaurantsController < ApplicationController 
+    def new
+        @restaurant = Restaurant.new
+    end
+
+    def create
+        @restaurant = Restaurant.new(restaurant_params)
+        @restaurant.user_id = session[:user_id]
+
+  
+        if @restaurant.save
+            redirect_to restaurant_path(@restaurant)
+        else 
+            flash[:error] = "Please Try Again"
+            render :new
+        end
+    end
+
+    def index
+        if params[:category_id]
+            category = Category.find(params[:id])
+            @restaurants = category.restaurants
+        end
+    end
 
 
 
