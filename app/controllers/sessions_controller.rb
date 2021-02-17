d99e2cbc0a280fe414b2cd77bf8b9f1a4db4092c
samara-amanda <samara.amanda01@gmail.com>
2021-02-17 09:49:33 -0500

    #new, #create, #destroy

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,25 @@
 class SessionsController < ApplicationController
 
+    def current_user
+        session[:user_id]
+    end
+
+    def new
+    end
     
+    def create
+        if !params[:username].nil? && !params[:username].empty?
+            session[:username] = params[:username]
+            redirect_to '/'
+        else
+            redirect_to login_path
+        end
+    end
+
+    def destroy
+        session.delete.username
+        redirect_to '/'
+    end
 
 
 end
