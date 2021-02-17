4ec5a8268836c1870f7ce89f602c92096753b7a9
samara-amanda <samara.amanda01@gmail.com>
2021-02-17 09:50:33 -0500

    changed variable to users_path

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,11 +6,12 @@ class UsersController < ApplicationController
 
     def create
         @user = User.new(user_params)
+        #if the user exists in the db
         if @user.save
             #log them in
             session[:user_id] = @user.id
             #redirect to the show page
-            redirect_to @user
+            redirect_to users_path
         else 
             flash[:error] = "Please Try Again"
             render :new
@@ -29,7 +30,7 @@ class UsersController < ApplicationController
     private
 
     def user_params
-        params.require(:user).permit(:name, :email, :password_digest)
+        params.require(:user).permit(:username, :email, :password_digest)
     end
 
 end
