ceaf3fcae6dffbbb4f781267326cb9145b5ba93c
samara-amanda <samara.amanda01@gmail.com>
2021-02-17 06:26:36 -0500

    #new, #create, #show, #user_params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,29 @@
 class UsersController < ApplicationController
 
+    def new
+        @user = User.new
+    end
+
+    def create
+        @user = User.new(user_params)
+        if @user.valid?
+            @user.save
+            session[:user_id] = @user.user_id
+            redirect_to user_path(@user)
+        else 
+            flash[:error] = "Please Try Again"
+            render :new
+        end
+    end
+
+    def show 
+        @user = User.find_by(params[:id]) unless !@user 
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:name, :email, :password_digest)
 
 
 end
