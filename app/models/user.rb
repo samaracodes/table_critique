e92210f37cf35f8261170d9c55d1d760fc43328e
samara-amanda <samara.amanda01@gmail.com>
2021-02-17 09:05:21 -0500

    added has_secure_password to store and authenticate user login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
     has_many :reviews
-    has_many :reviewed_restaurant, through: :reviews, source: :restaurant
+    has_many :reviewed_restaurant, through: :reviews
     has_many :restaurants
 
 end
