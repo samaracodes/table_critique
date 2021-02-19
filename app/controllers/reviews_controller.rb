a026c258d9c91273e31421a0932dc201f40b0e67
samara-amanda <samara.amanda01@gmail.com>
2021-02-19 14:31:15 -0500

    form association, #show find by ID, review params updated

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -11,23 +11,24 @@ class ReviewsController < ApplicationController
 
    def create
       @review = current_user.reviews.build(review_params)
+      @restaurant = Restaurant.find_or_create_by(name: params[:review][:restaurant_name])
         #if the post exists in the db
       if @review.save
       #redirect to the show page
-         
          redirect_to reviews_path
+         
       else 
          render :new
       end
    end
 
    def show
-      @review = Review.find_by(params[:id])
+      @review = Review.find_by_id(params[:id])
    end
 
    private
    
    def review_params
-      params.require(:review).permit(:title, :content)
+      params.require(:review).permit(:title, :content, :restaurant_name)
    end
 end
