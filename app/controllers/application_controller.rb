4f4b355e41ab099ce2147d9e6cfbf60556a85548
samara-amanda <samara.amanda01@gmail.com>
2021-02-18 21:52:14 -0500

    helpers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,22 @@
 class ApplicationController < ActionController::Base
+    def current_user
+        #we dont user @user because it might override the user 
+        # variable in the controller
 
+        # 1. if there is a session[:user_id] proceed.. 
+        # ....(we dont want to make a call to the database 
+        # & not find anything) 
+        # 2. if @current_user already exists then set the user to @current_user
+        # 3. if not, find user by user_id in the users table and set 
+        # to @current_user. 
+        @current_user ||= User.find_by_id(session[:user_id]) if session[:user_id]
+    end
+
+    def logged_in?
+        !!session[:user_id]
+    end
+
+    def redirect_if_not_logged_in
+        redirect_to '/' if !logged_in?
+    end
 end
