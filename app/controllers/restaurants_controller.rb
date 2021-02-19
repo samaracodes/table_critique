e5dad3eb737eb5635d49f967e1db23aef2eb2009
samara-amanda <samara.amanda01@gmail.com>
2021-02-19 02:43:37 -0500

    #index

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,5 +1,20 @@
 class RestaurantsController < ApplicationController 
 
+    def index
+        if params[:review_id]
+            #nested
+            @review = Review.find_by_id(params[:review_id])
+            if @review
+                @restaurant = @review.restaurant
+            else
+                flash[:message] = "That review doesn't exist"
+                @restaurants = Restaurant.all
+            end
+        else 
+            @restaurants = Restaurant.all
+
+        end
+    end
 
 
 end
