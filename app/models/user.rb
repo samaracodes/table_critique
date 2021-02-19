2174e6031ce39f36108cf65b2cefdcbc494eb6cd
samara-amanda <samara.amanda01@gmail.com>
2021-02-19 02:45:43 -0500

    has many through

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
     has_many :reviews
     has_many :reviewed_restaurant, through: :reviews
-    has_many :restaurants
+    has_many :restaurants, through: :reviews
     has_secure_password #authenticate, validate
+
 
 end
