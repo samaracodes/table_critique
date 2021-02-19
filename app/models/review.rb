070cc1f20c010f0a68d06b5b1fd6dfb5c202258e
samara-amanda <samara.amanda01@gmail.com>
2021-02-19 06:43:56 -0500

    edit relations, defined categories attribute method

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,14 @@
 class Review < ApplicationRecord
     belongs_to :user
-    has_one :restaurant
-    has_one :category, through: :restaurants
- 
+    belongs_to :restaurant
+    has_many :categories, through: :review_categories
+    has_many :review_categories, through: :restaurants
+
+    def categories_attributes=(category_attributes)
+        category_attributes.values.each do |category_attribute|
+            category = Category.find_or_create_by(category_attribute) unless category_attribute[:name].blank?
+            self.categories << category if category
+        end
+    end
+    
 end
