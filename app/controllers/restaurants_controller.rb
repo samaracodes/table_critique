218c34267a3e1cc47a1672a003565e6dbf3d8845
samara-amanda <samara.amanda01@gmail.com>
2021-02-18 21:01:00 -0500

    removed prev code

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,36 +1,4 @@
 class RestaurantsController < ApplicationController 
-    def new
-        @restaurant = Restaurant.new
-    end
-
-    def show
-        @restaurants = Restaurant.find(params[:id])
-    end
-
-    def create
-        @restaurant = Restaurant.new(restaurant_params)
-        @restaurant.user_id = session[:user_id]
-
-  
-        if @restaurant.save
-            redirect_to restaurant_path(@restaurant)
-        else 
-            flash[:error] = "Please Try Again"
-            render :new
-        end
-    end
-
-    def index
-        if params[:category_id]
-            @restaurants = Category.find(params[:id]).restaurants
-        else
-            @restaurants.all
-        end
-    end
-
-    def update 
-
-    end
 
 
 
