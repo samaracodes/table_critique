0d9c2c8a4a58592fc43792224ba554b05d0dbade
samara-amanda <samara.amanda01@gmail.com>
2021-02-18 20:56:29 -0500

    has_secure_password

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,6 @@ class User < ApplicationRecord
     has_many :reviews
     has_many :reviewed_restaurant, through: :reviews
     has_many :restaurants
+    has_secure_password #authenticate, validate
 
 end
