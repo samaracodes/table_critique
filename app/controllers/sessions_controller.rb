d423e89fd3df38fadc115920f69c0219482daf6b
samara-amanda <samara.amanda01@gmail.com>
2021-02-18 20:59:48 -0500

    removed prev code

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,27 +1,14 @@
 class SessionsController < ApplicationController
 
-    def current_user
-        session[:user_id]
-    end
-
-    def new
-    end
-    
-    def create
-        if !params[:username].nil? && !params[:username].empty?
-            session[:username] = params[:username]
-            redirect_to '/'
-        else
-            redirect_to login_path
-        end
+    def home
     end
 
     def destroy
-        session.delete.username
-        redirect_to '/'
+        session.clear
+        redirect_to root_path
     end
 
-    def home
+    def new
     end
 
 
