d68df93002ca66587151077e404fb0be4102516b
samara-amanda <samara.amanda01@gmail.com>
2021-02-19 06:43:04 -0500

    edited has many relations

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,5 +1,7 @@
 class Restaurant < ApplicationRecord
-    belongs_to :category
     belongs_to :review
+    has_many :review_categories
+    has_many :categories, through: :review_categories
+
 
 end
