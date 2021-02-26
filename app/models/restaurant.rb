42b907bd327eaf7f667974844f0f27fd40c79baa
samara-amanda <samara.amanda01@gmail.com>
2021-02-26 06:44:47 -0500

    self.search method

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,6 +2,12 @@ class Restaurant < ApplicationRecord
     has_many :reviews
     has_many :restaurant_categories
     has_many :categories, through: :restaurant_categories
-
-
+    
+    def self.search(search)
+        if search.present?
+            where('name LIKE ?', "%#{search}%")
+        else
+            where(true)
+        end
+    end
 end
