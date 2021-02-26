62a55f9580d8dcec53138ec96149da4076bc0fbe
samara-amanda <samara.amanda01@gmail.com>
2021-02-26 06:30:32 -0500

    #index, #new, #create, #category_params

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,37 +3,4 @@ class CategoriesController < ApplicationController
         @category = Category.find(params[:id])
     end
 
-    def index
-        if params[:restaurant_id]
-            #nested
-            @restaurant = Restaurant.find_by_id(params[:restaurant_id])
-            if @restaurant
-                @category = @restaurant.category
-            else
-                flash[:message] = "That restaurant doesn't exist"
-                @restaurants = Restaurant.all
-            end
-        else 
-            @restaurants = Restaurant.all
-
-        end
-    end
-  
-     def new
-        @category = Category.new
-     end
-  
-     def create
-        if @category.blank?
-            @category = Category.find_or_create_by(id: params[:id])
-        end
-    end
-  
-     private
-     
-     def category_params
-        params.require(:category).permit(:name, restaurant_ids: [])
-     end    
-
-
 end
