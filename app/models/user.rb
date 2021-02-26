683cc23eb8b53a293be960b900a3ee8f7f07905d
samara-amanda <samara.amanda01@gmail.com>
2021-02-26 06:46:29 -0500

    association fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,5 @@
 class User < ApplicationRecord
     has_many :reviews
-    has_many :reviewed_restaurant, through: :reviews
-    has_many :restaurants, through: :reviews
     has_secure_password #authenticate, validate
 
 
