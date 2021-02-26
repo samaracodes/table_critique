b1c162e15d025368f4314dc4ec8305f7179a6786
samara-amanda <samara.amanda01@gmail.com>
2021-02-25 23:55:12 -0500

    seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Category.create(name: "Caribbean")
+Category.create(name: "Seafood")
+Category.create(name: "Asian")
+Category.create(name: "Italian")
+Category.create(name: "Steakhouse")
+Category.create(name: "American")
+Category.create(name: "Latin")
+Category.create(name: "Indian")
+Category.create(name: "Fast Food")
+Category.create(name: "Dessert")
+
