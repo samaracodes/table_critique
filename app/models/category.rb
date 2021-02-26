fd1c2e3be1fba8be02f8187bb4386a968e74a8e5
samara-amanda <samara.amanda01@gmail.com>
2021-02-26 06:44:18 -0500

    has_many assc fix

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,6 @@
 class Category < ApplicationRecord
     has_many :restaurant_categories
-    has_many :reviews, through: :restaurant_categories
+    has_many :restaurants, through: :restaurant_categories
 
 
 end
