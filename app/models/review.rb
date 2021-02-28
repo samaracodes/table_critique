2c61da16aec05d69df41f644c19043c3e19519b4
Sam <samara.amanda01@gmail.com>
2021-02-28 02:18:42 -0500

    validations

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,14 +1,18 @@
 class Review < ApplicationRecord
-    belongs_to :user
-    belongs_to :restaurant
+  belongs_to :user
+  belongs_to :restaurant
+  validates :title, :content, :restaurant, presence: true
 
-    #scope method
-    scope :recently_created, -> { order(created_at: :desc) }
+
+
+  #scope method
+  scope :recently_created, -> { order(created_at: :desc) }
     
-
   def restaurant_attributes=(restaurant_attributes)
     self.restaurant = Restaurant.find_or_create_by(name: restaurant_attributes[:name]) do |r|
-        r.category_id = restaurant_attributes[:category_id]
+      r.category_id = Category.find_by(id: restaurant_attributes[:category_id])
     end
   end
+
+
 end
