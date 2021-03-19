af2d689c046732c68b7c2dd8a71573d0f73fc638
Sam <samara.amanda01@gmail.com>
2021-03-19 12:54:06 -0400

    removed miscellaneous associations and validations

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,9 +1,6 @@
 class Category < ApplicationRecord
-    has_many :reviews
     has_many :review_categories
     has_many :reviews, through: :review_categories
-    belongs_to :review, optional: true
-    validates :name, presence: true
 
 
     
