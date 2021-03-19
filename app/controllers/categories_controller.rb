2a37e5591058aab879a971ad86289c665015f85f
Sam <samara.amanda01@gmail.com>
2021-03-19 12:50:42 -0400

    created #index #show metods

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,21 @@
 class CategoriesController < ApplicationController
-   
+
+    def index
+        if params[:review_id]
+            @categories = Review.find_by(params[:review_id]).categories
+        else
+            @categories = Category.all
+        end
+    end
+
+    def show
+        @category = Category.find_by_id(params[:id])
+    end
+     
+    private
+     
+    def category_params
+        params.require(:category).permit(:name)
+    end
 
 end
