161f7540f3d2a740d62b0212cc45bdcf9b8aa9b8
Sam <samara.amanda01@gmail.com>
2021-03-19 12:54:31 -0400

    no changes

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,6 +1,6 @@
 class Restaurant < ApplicationRecord
-    has_many :reviews
     has_many :users, through: :reviews
+    has_many :reviews
     belongs_to :review, optional: true
     validates :name, presence: true
     
