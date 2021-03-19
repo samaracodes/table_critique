4a242a9ed3525ce13b3863a3c734a1979887bd6e
Sam <samara.amanda01@gmail.com>
2021-03-19 01:20:52 -0400

    assoc updates, validations, category_attr addition, & rdried up code in restaurant_attr

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,18 +1,24 @@
 class Review < ApplicationRecord
   belongs_to :user
   belongs_to :restaurant
-  validates :title, :content, :restaurant, presence: true
-
+  belongs_to :category
+  has_many :review_categories
+  has_many :categories, through: :review_categories
+  accepts_nested_attributes_for :restaurant
+  accepts_nested_attributes_for :category
+  validates :title, :content, presence: true
+  
 
 
   #scope method
   scope :recently_created, -> { order(created_at: :desc) }
     
   def restaurant_attributes=(restaurant_attributes)
-    self.restaurant = Restaurant.find_or_create_by(name: restaurant_attributes[:name]) do |r|
-      r.category_id = Category.find_by(id: restaurant_attributes[:category_id])
-    end
+    self.restaurant = Restaurant.find_or_create_by(name: restaurant_attributes[:name])
   end
 
+  def category_attributes=(category_attributes)
+      self.category = Category.find_or_create_by(name: category_attributes[:name])
+  end
 
 end
