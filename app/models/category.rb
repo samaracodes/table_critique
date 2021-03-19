cb8cdbe0371179cf3a836b2735ad1a72da00c004
Sam <samara.amanda01@gmail.com>
2021-03-19 02:05:16 -0400

    association addition

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,5 @@
 class Category < ApplicationRecord
+    has_many :reviews
     has_many :review_categories
     has_many :reviews, through: :review_categories
     belongs_to :review, optional: true
